88901a0575ed070042d9b245eb8e793510475007
AndreyLevin1780 <andreylevin1980@mail.ru>
2024-06-07 21:42:15 +0300

    work branch

---
+++ src/main/java/pro/sky/calculatortest/CalculatortestApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatortestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatortestApplication.class, args);
+	}
+
+}
