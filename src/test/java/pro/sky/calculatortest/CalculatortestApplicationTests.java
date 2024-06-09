40dbaae524e6a5fcfc6fe72a68da31eae19218a3
AndreyLevin1780 <andreylevin1980@mail.ru>
2024-06-09 22:25:50 +0300

    work branch

--- src/test/java/pro/sky/calculatortest/CalculatortestApplicationTests.java
+++ src/test/java/pro/sky/calculatortest/CalculatortestApplicationTests.java
@@ -1,13 +1,57 @@
 package pro.sky.calculatortest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import pro.sky.calculatortest.exception.DivisionByZeroException;
+import pro.sky.calculatortest.service.CalculatorService;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CalculatortestApplicationTests {
 
+	CalculatorService calculatorService = new CalculatorService();
 	@Test
-	void contextLoads() {
+	void plus() {
+
+		int result = calculatorService.plus(10, 20);
+		assertEquals(30, result);
+
+		result = calculatorService.plus(10, -20);
+		assertEquals(-10, result);
+	}
+
+	@Test
+	void minus() {
+		int result = calculatorService.minus(30, 10);
+		assertEquals(20, result);
+
+		result = calculatorService.minus(130, 170);
+		assertEquals(-40, result);
+	}
+
+	@Test
+	void multiply() {
+		int result = calculatorService.multiply(-2, 30);
+		assertEquals(-60, result);
+
+		result = calculatorService.multiply(2, 40);
+		assertEquals(80, result);
+	}
+
+	@Test
+	void divide() {
+		int result = calculatorService.divide(20, 10);
+		assertEquals(2, result);
+
+		result = calculatorService.divide(-2000, -100);
+		assertEquals(20, result);
+	}
+
+	@Test
+	void divideNegativeTest() {
+		assertThrows(ArithmeticException.class, () -> calculatorService.divide(10,0));
 	}
 
 }
